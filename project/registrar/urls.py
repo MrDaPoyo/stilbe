33e667aabf81c83b4b14a211843d623936a98d70
Luca Tamagnini <151396032+MrDaPoyo@users.noreply.github.com>
2024-04-14 17:11:12 +0000

    Signup!

---
+++ project/registrar/urls.py
@@ -0,0 +1,8 @@
+from .views import RegisterView
+from django.urls import path, include
+
+urlpatterns = [
+    # other urls...
+    path('register/', RegisterView.as_view(), name='register'),
+    path('', include("django.contrib.auth.urls"))
+]
--- project/registrar/views.py
+++ project/registrar/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 
-# Create your views here.
+class RegisterForm(UserCreationForm):
+    pass
+
+class RegisterView(CreateView):
+    form_class = RegisterForm
+    template_name = 'signup.html'
+    success_url = reverse_lazy('login')
