46bec4b0a79dbfab54a5322d51ab409dda9c77a5
Luca Tamagnini <151396032+MrDaPoyo@users.noreply.github.com>
2024-04-14 17:38:18 +0000

    Add redirect to login page in home URL

--- project/registrar/urls.py
+++ project/registrar/urls.py
@@ -1,8 +1,10 @@
 from .views import RegisterView
 from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
     # other urls...
     path('register/', RegisterView.as_view(), name='register'),
+    path('', lambda request: redirect('login'), name='home'),
     path('', include("django.contrib.auth.urls")),
 ]
