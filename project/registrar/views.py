d3ddd077c223380b0a7089c643d8c92b17d381f7
Luca Tamagnini <151396032+MrDaPoyo@users.noreply.github.com>
2024-04-14 17:25:28 +0000

    register and login

--- project/registrar/views.py
+++ project/registrar/views.py
@@ -1,12 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic.edit import CreateView
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth import logout
 
+# Create your views here.
 class RegisterForm(UserCreationForm):
     pass
 
-class RegisterView(CreateView):
+class RegisterView(LoginRequiredMixin, CreateView):
     form_class = RegisterForm
     template_name = 'signup.html'
     success_url = reverse_lazy('login')
+    login_url = '/login/'  # Specify the login URL
+
+    def dispatch(self, request, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return super().dispatch(request, *args, **kwargs)
+        return self.handle_no_permission()
+    
+
+def logout_view(request):
+    logout(request)
+    return render('logout.html'), redirect('login')
