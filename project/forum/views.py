6d205f62c79834de5075231a85063cc324c0c8c5
Luca Tamagnini <151396032+MrDaPoyo@users.noreply.github.com>
2024-04-15 08:29:25 +0000

    Refactor login and registration functionality in views.py and templates

--- project/forum/views.py
+++ project/forum/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import LoginView
+from django.contrib.auth import get_user_model
+
 # Create your views here.
 @login_required
 def home(request):
-    return render(request, 'forum/home.html')
+    User = get_user_model()
+    users = User.objects.all() 
+    return render (request, "forum/home.html", {"users":users})
--- project/registrar/views.py
+++ project/registrar/views.py
@@ -5,28 +5,10 @@ from django.urls import reverse_lazy
 from django.contrib.auth.mixins import AccessMixin
 from django.core.exceptions import PermissionDenied
 
-class AnonymousRequiredMixin(AccessMixin):
-    """Verify that the current user is not authenticated."""
-    def dispatch(self, request, *args, **kwargs):
-        if request.user.is_authenticated:
-            return self.handle_no_permission()
-        return super().dispatch(request, *args, **kwargs)
-
-    def handle_no_permission(self):
-        if self.raise_exception:
-            raise PermissionDenied(self.get_permission_denied_message())
-        return redirect("home")
-
 class RegisterForm(UserCreationForm):
     pass
 
-class RegisterView(AnonymousRequiredMixin, CreateView):
+class RegisterView(CreateView):
     form_class = RegisterForm
     template_name = 'signup.html'
     success_url = reverse_lazy('login')
-    login_url = '/login/'  # Specify the login URL
-
-    def dispatch(self, request, *args, **kwargs):
-        if self.request.user.is_authenticated:
-            return super().dispatch(request, *args, **kwargs)
-        return self.handle_no_permission()
