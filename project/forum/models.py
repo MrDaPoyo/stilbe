4641d2a59124c4f02d4a6d17722f71f860517f94
Luca Tamagnini <151396032+MrDaPoyo@users.noreply.github.com>
2024-04-15 19:18:22 +0000

    Add Thread and Post models to forum/models.py

--- project/forum/models.py
+++ project/forum/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Thread():
+    title = models.CharField(max_length=100)
+    content = models.TextField(max_length=1000)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+class Post():
+    thread = models.ForeignKey(Thread, on_delete=models.CASCADE)
+    content = models.TextField(max_length=1000)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.content
