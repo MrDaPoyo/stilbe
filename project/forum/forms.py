c496e5e0ada6b910fdce99ab345f8ef997be091d
Luca Tamagnini <151396032+MrDaPoyo@users.noreply.github.com>
2024-04-15 20:44:18 +0000

    Add required attribute and placeholder to content field in ThreadCreationForm

--- project/forum/forms.py
+++ project/forum/forms.py
@@ -4,6 +4,7 @@ from .models import Thread, Post
 class ThreadCreationForm(forms.ModelForm):
     class Meta:
         model = Thread
+        content = forms.CharField(required=True, widget=forms.Textarea(attrs={'placeholder': 'Write your initial post here...', 'class': 'form-control'}))
         fields = ['title', 'content']
 
 class PostCreationForm(forms.ModelForm):
