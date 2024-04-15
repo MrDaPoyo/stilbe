f75e343e667f2700ce9327f646b2c280044723fb
Luca Tamagnini <151396032+MrDaPoyo@users.noreply.github.com>
2024-04-15 13:31:27 +0000

    Refactor login and registration functionality in urls.py

--- project/registrar/urls.py
+++ project/registrar/urls.py
@@ -2,11 +2,13 @@ from .views import RegisterView
 from django.urls import path, include
 from django.shortcuts import redirect
 from django.contrib.auth.views import LogoutView
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     # other urls...
-    path('register/', RegisterView.as_view(), name='register'),
-    path('', lambda request: redirect('login'), name='home'),
-    path('', include(("django.contrib.auth.urls", "auth"))),
+    path('register/', RegisterView.as_view(redirect_authenticated_user=True), name='register'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    path("login/", auth_views.LoginView.as_view(redirect_authenticated_user=True)),
+    path('', include(("django.contrib.auth.urls", "auth"))),
+    
 ]
