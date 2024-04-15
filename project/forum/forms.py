de58c065fb1aca1abb97839b9f2c2ac3fb586a1d
Luca Tamagnini <151396032+MrDaPoyo@users.noreply.github.com>
2024-04-15 20:45:21 +0000

    Update ThreadCreationForm to include required attribute and placeholder for title field

--- project/forum/forms.py
+++ project/forum/forms.py
@@ -2,13 +2,11 @@ from django import forms
 from .models import Thread, Post
 
 class ThreadCreationForm(forms.ModelForm):
+    title = forms.CharField(required=True, widget=forms.TextInput(attrs={'placeholder': 'Title', 'class': 'form-control'}))
+    content = forms.CharField(required=True, widget=forms.Textarea(attrs={'placeholder': 'Write your initial post here...', 'class': 'form-control'}))
+    
     class Meta:
         model = Thread
-        content = forms.CharField(required=True, widget=forms.Textarea(attrs={'placeholder': 'Write your initial post here...', 'class': 'form-control'}))
         fields = ['title', 'content']
 
-class PostCreationForm(forms.ModelForm):
-    class Meta:
-        model = Post
-        fields = ['content']
         
