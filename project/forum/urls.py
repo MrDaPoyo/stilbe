04a5ec656fe6933c44b391c77fdb497645c1a446
Luca Tamagnini <151396032+MrDaPoyo@users.noreply.github.com>
2024-04-16 07:47:14 +0000

    Add threadView function to handle thread view in forum/views.py

--- project/forum/urls.py
+++ project/forum/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('home', views.home, name='home'),
+    path('thread/<int:pk>', views.threadView, name='thread'),
     # Add more URL patterns here
 ]
--- project/forum/views.py
+++ project/forum/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import LoginView
 from django.contrib.auth import get_user_model
+from .models import Thread
+
 
 # Create your views here.
 @login_required
@@ -9,3 +11,10 @@ def home(request):
     User = get_user_model()
     users = User.objects.all() 
     return render (request, "forum/home.html", {"users":users})
+
+
+def threadView(request, pk):
+    thread = Thread.objects.get(id=pk)
+    title = thread.title
+    content = thread.content
+    return render(request, "forum/thread.html", {"title": title, "content": content})
