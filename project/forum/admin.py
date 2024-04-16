b2d5a6fb9639081679f0334448793c1e9ff82106
Luca Tamagnini <151396032+MrDaPoyo@users.noreply.github.com>
2024-04-16 06:46:44 +0000

    Register Thread and Post models in admin.py

--- project/forum/admin.py
+++ project/forum/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Thread, Post
+from django.contrib.auth.models import User
 
 # Register your models here.
+
+admin.site.register(Thread)
+admin.site.register(Post)
