d872cc0f4b55c9d01f6dcdfebbbe58ad4e445a04
Luca Tamagnini <151396032+MrDaPoyo@users.noreply.github.com>
2024-04-16 06:43:37 +0000

    Add user field to Thread and Post models in forum/models.py

--- project/forum/models.py
+++ project/forum/models.py
@@ -3,21 +3,22 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Thread(models.Model):
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING, default="")
     title = models.CharField(max_length=100)
     content = models.TextField(max_length=1000)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
 
     def __str__(self):
         return self.title
 
 class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default="")
     thread = models.ForeignKey(Thread, on_delete=models.CASCADE)
     content = models.TextField(max_length=1000)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.content
