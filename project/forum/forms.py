53fc211aee6de5abdc043333b28ef1a4d9c7f985
Luca Tamagnini <151396032+MrDaPoyo@users.noreply.github.com>
2024-04-17 13:31:42 +0000

    Update PostCreationForm and ThreadCreationForm in forum/forms.py

--- project/forum/forms.py
+++ project/forum/forms.py
@@ -3,10 +3,15 @@ from django.forms.widgets import HiddenInput
 from .models import Thread, Post
 
 class PostCreationForm(forms.ModelForm):
-    content = forms.CharField(required=True, widget=forms.Textarea(attrs={'placeholder': 'Write your initial post here...', 'class': 'form-control'}))
+    content = forms.CharField(required=True, widget=forms.Textarea(attrs={'placeholder': 'Reply here.', 'class': 'form-control'}))
     
     class Meta:
         model = Post
         fields = ['content']
 
-        
+class ThreadCreationForm(forms.ModelForm):
+    content = forms.CharField(required=True, widget=forms.Textarea(attrs={'placeholder': 'Create a thread.', 'class': 'form-control'}))
+    
+    class Meta:
+        model = Thread
+        fields = ['content']
--- project/forum/models.py
+++ project/forum/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Thread(models.Model):
-    user = models.ForeignKey(User, on_delete=models.DO_NOTHING, default="")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default="")
     title = models.CharField(max_length=100)
     content = models.TextField(max_length=1000)
     created_at = models.DateTimeField(auto_now_add=True)
