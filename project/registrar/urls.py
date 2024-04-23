f38ca1ba18997f5b056fa13840edd12138cf99ef
Luca Tamagnini <151396032+MrDaPoyo@users.noreply.github.com>
2024-04-23 11:52:52 +0200

    Update urls.py

--- project/registrar/urls.py
+++ project/registrar/urls.py
@@ -6,9 +6,10 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     # other urls...
+    path('', include('captcha.urls')),
     path('register/', RegisterView.as_view(redirect_authenticated_user=True), name='register'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path("login/", auth_views.LoginView.as_view(redirect_authenticated_user=True)),
-    path('', include(("django.contrib.auth.urls", "auth"))),  
-    path('captcha/', include('captcha.urls')),
+    path('auth/', include(("django.contrib.auth.urls", "auth"))),  
+    
 ]
